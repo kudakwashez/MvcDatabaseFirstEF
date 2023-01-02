8ec5d1cffce61bf1908c4a22c77acd898273cc17
Eng Kadebu <Eng Kadebu@DESKTOP-04NEVUL>
2023-01-02 16:52:42 +0200

    Add project files.

---
+++ MvcDatabaseFirstEF/Controllers/Default1Controller.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcDatabaseFirstEF.Models;
+
+namespace MvcDatabaseFirstEF.Controllers
+{
+    public class Default1Controller : Controller
+    {
+        // GET: Default1
+        public ActionResult Index()
+
+        {
+
+            using (var ctx = new EmployeeDBEntities())
+
+            {
+
+                List<EmployeeDetail> Employeelist = ctx.EmployeeDetails.ToList();
+
+                return View(Employeelist);
+
+            }
+
+        }
+    }
+}
---
+++ MvcDatabaseFirstEF/Controllers/DefaultController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcDatabaseFirstEF.Models;
+
+namespace MvcDatabaseFirstEF.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+
+        {
+
+            using (var ctx = new EmployeeDBEntities1())
+
+            {
+
+                List<EmployeeDetail> Employeelist = ctx.EmployeeDetails.ToList();
+
+                return View(Employeelist);
+
+            }
+
+        }
+    }
+}
